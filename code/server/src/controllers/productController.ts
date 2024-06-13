332123904870b975ad12b05c9cc9d7bc0a5e5ee4
Gino Marco Fachechi <s328689@studenti.polito.it>
2024-06-13 12:30:03 +0200

    Update file productController.ts

--- code/server/src/controllers/productController.ts
+++ code/server/src/controllers/productController.ts
@@ -1,93 +1,431 @@
-import  {Product}  from "../components/product";
-import ProductDAO from "../dao/productDAO";
+import db from "../db/db"
+import { Product } from "../components/product"
+import { ProductNotFoundError, ProductAlreadyExistsError, ProductSoldError, EmptyProductStockError, LowProductStockError, ProductInvalidDate, ProductInvalidGrouping} from "../errors/productError";
 
 /**
- * Represents a controller for managing products.
- * All methods of this class must interact with the corresponding DAO class to retrieve or store data.
+ * A class that implements the interaction with the database for all product-related operations.
+ * You are free to implement any method you need here, as long as the requirements are satisfied.
  */
-class ProductController {
-    private dao: ProductDAO
-
-    constructor() {
-        this.dao = new ProductDAO
+class ProductDAO {
+	registerProducts(model: string, category: string, quantity: number, details: string | null, sellingPrice: number, arrivalDate: string | null): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            try {
+				const currentD: Date = new Date();
+				currentD.setHours(12, 0, 0, 0);
+				if (!arrivalDate) {
+					arrivalDate = currentD.toISOString().split('T')[0];
+				}
+				const arrivalD: Date = new Date(arrivalDate);
+				if (arrivalD > currentD) {
+					reject(new ProductInvalidDate())
+					return;
+				}
+                const sql = "INSERT INTO products(model, category, quantity, details, sellingPrice, arrivalDate) VALUES(?, ?, ?, ?, ?, ?)"
+                db.run(sql, [model, category, quantity, details, sellingPrice, arrivalDate], (err: Error | null) => {
+                    if (err) {
+                        if (err.message.includes("UNIQUE constraint failed: product.model")) {
+							reject(new ProductAlreadyExistsError())
+							return;
+						}
+                        reject(err)
+						return;
+                    }
+                    else {
+						resolve(true);
+						return;
+					}
+                })
+            } catch (error) {
+                reject(error)
+            }
+        })
     }
 
-    /**
-     * Registers a new product concept (model, with quantity defining the number of units available) in the database.
-     * @param model The unique model of the product.
-     * @param category The category of the product.
-     * @param quantity The number of units of the new product.
-     * @param details The optional details of the product.
-     * @param sellingPrice The price at which one unit of the product is sold.
-     * @param arrivalDate The optional date in which the product arrived.
-     * @returns A Promise that resolves to nothing.
-     */
-    async registerProducts(model: string, category: string, quantity: number, details: string | null, sellingPrice: number, arrivalDate: string | null) :Promise<boolean> {
-		return await this.dao.registerProducts(model, category, quantity, details, sellingPrice, arrivalDate)
+
+	changeProductQuantity(model: string, newQuantity: number, changeDate: string | null): Promise<number> {
+        return new Promise<number>((resolve, reject) => {
+            try {
+                const sql = "SELECT arrivalDate, quantity FROM products WHERE model = ?"
+                db.get(sql, [model], (err: Error | null, row: any) => {
+                    if (err) {
+						reject(err)
+						return;
+                    }
+					if (!row) {
+						reject(new ProductNotFoundError())
+						return;
+					}
+					const currentD: Date = new Date();
+					currentD.setHours(12, 0, 0, 0);
+					if (!changeDate) {
+						changeDate = currentD.toISOString().split('T')[0];
+					}
+					const changeD: Date = new Date(changeDate);
+					const arrivalD: Date = new Date(row.arrivalDate);
+					if (changeD < arrivalD) {
+						reject(new ProductInvalidDate())
+						return;
+					}
+					if (changeD > currentD) {
+						reject(new ProductInvalidDate())
+						return;
+					}
+                    else {
+						const sql = "UPDATE products SET quantity = quantity + ?, arrivalDate = ? WHERE model = ?"
+						db.run(sql, [newQuantity, changeDate, model], (err: Error | null) => {
+							if (err) {
+								reject(err)
+								return;
+							}
+							else {
+								resolve(row.quantity + newQuantity);
+								return;
+							}
+						})
+					}
+                })
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+	
+	
+	sellProduct(model: string, quantity: number, sellingDate: string | null): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            try {
+                const sql = "SELECT arrivalDate, quantity FROM products WHERE model = ?"
+                db.get(sql, [model], (err: Error | null, row: any) => {
+                    if (err) {
+						reject(err)
+						return;
+                    }
+					if (!row) {
+						reject(new ProductNotFoundError())
+						return;
+					}
+					if (row.quantity == 0) {
+						reject(new EmptyProductStockError())
+						return;
+					}
+					if (row.quantity < quantity) {
+						reject(new LowProductStockError())
+						return;
+					}
+					const currentD: Date = new Date();
+					currentD.setHours(12, 0, 0, 0);
+					if (!sellingDate) {
+						sellingDate = currentD.toISOString().split('T')[0];
+					}
+					const sellingD: Date = new Date(sellingDate);
+					const arrivalD: Date = new Date(row.arrivalDate);
+					if (sellingD < arrivalD) {
+						reject(new ProductInvalidDate())
+						return;
+					}
+					if (sellingD > currentD) {
+						reject(new ProductInvalidDate())
+						return;
+					}
+                    else {
+						const sql = "UPDATE products SET quantity = quantity - ? WHERE model = ?"
+						db.run(sql, [quantity, model], (err: Error | null) => {
+							if (err) {
+								reject(err)
+								return;
+							}
+							else {
+								resolve(true);
+								return;
+							}
+						})
+					}
+                })
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+	
+	
+	getProducts(grouping: string | null, category: string | null, model: string | null): Promise<Product[]> {
+        return new Promise<Product[]>((resolve, reject) => {
+            try {
+				let products: Product[] = [];
+				if (grouping == "model") {
+					if (!model || category) {
+						reject(new ProductInvalidGrouping())
+						return;
+					}
+					const sql = "SELECT * FROM products WHERE model = ?"
+					db.get(sql, [model], (err: Error | null, row: any) => {
+						if (err) {
+							reject(err)
+							return;
+						}
+						if (!row) {
+							reject(new ProductNotFoundError())
+							return;
+						}
+						else {
+							const product = new Product(
+										row.sellingPrice,
+										row.model,
+										row.category,
+										row.arrivalDate,
+										row.details,
+										row.quantity,
+								);
+							products.push(product);
+							resolve(products)
+							return;
+						}
+					})
+				} else if (grouping == "category") {
+					if (model || !category) {
+							reject(new ProductInvalidGrouping())
+							return;
+					}
+					const sql = "SELECT * FROM products WHERE category = ?"
+					db.all(sql, [category], (err: Error | null, rows: any[]) => {
+						if (err) {
+							reject(err)
+							return;
+						}
+						else {
+							rows.forEach(function (row) {
+								const product = new Product(
+									row.sellingPrice,
+									row.model,
+									row.category,
+									row.arrivalDate,
+									row.details,
+									row.quantity,
+								);
+								products.push(product);
+							}); 
+							resolve(products)
+							return;
+						}
+					})
+				} else {
+					if (model || category) {
+							reject(new ProductInvalidGrouping())
+							return;
+					}
+					const sql = "SELECT * FROM products"
+					db.all(sql, [], (err: Error | null, rows: any[]) => {
+						if (err) {
+							reject(err)
+							return;
+						}
+						else {
+							rows.forEach(function (row) {
+								const product = new Product(
+									row.sellingPrice,
+									row.model,
+									row.category,
+									row.arrivalDate,
+									row.details,
+									row.quantity,
+								);
+								products.push(product);
+							}); 
+							resolve(products)
+							return;
+						}
+					})
+				}
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+	
+	
+	getAvailableProducts(grouping: string | null, category: string | null, model: string | null): Promise<Product[]> {
+        return new Promise<Product[]>((resolve, reject) => {
+            try {
+				let products: Product[] = [];
+				if (grouping == "model") {
+					if (!model || category) {
+							reject(new ProductInvalidGrouping())
+							return;
+					}
+					const sql = "SELECT * FROM products WHERE model = ? AND quantity > 0"
+					db.get(sql, [model], (err: Error | null, row: any) => {
+						if (err) {
+							reject(err)
+							return;
+						}
+						if (!row) {
+							reject(new ProductNotFoundError())
+							return;
+						}
+						else {
+							const product = new Product(
+									row.sellingPrice,
+									row.model,
+									row.category,
+									row.arrivalDate,
+									row.details,
+									row.quantity,
+								);
+							products.push(product);
+							resolve(products)
+							return;
+						}
+					})
+				} else if (grouping == "category") {
+					if (model || !category) {
+							reject(new ProductInvalidGrouping())
+							return;
+					}
+					const sql = "SELECT * FROM products WHERE category = ? AND quantity > 0"
+					db.all(sql, [category], (err: Error | null, rows: any[]) => {
+						if (err) {
+							reject(err)
+							return;
+						}
+						else {
+							rows.forEach(function (row) {
+								const product = new Product(
+									row.sellingPrice,
+									row.model,
+									row.category,
+									row.arrivalDate,
+									row.details,
+									row.quantity,
+								);
+								products.push(product);
+							}); 
+							resolve(products)
+							return;
+						}
+					})
+				} else {
+					if (model || category) {
+							reject(new ProductInvalidGrouping())
+							return;
+					}
+					const sql = "SELECT * FROM products WHERE quantity > 0"
+					db.all(sql, [], (err: Error | null, rows: any[]) => {
+						if (err) {
+							reject(err)
+							return;
+						}
+						else {
+							rows.forEach(function (row) {
+								const product = new Product(
+									row.sellingPrice,
+									row.model,
+									row.category,
+									row.arrivalDate,
+									row.details,
+									row.quantity,
+								);
+								products.push(product);
+							}); 
+							resolve(products)
+							return;
+						}
+					})
+				}
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+	
+	
+	deleteProduct(model: string): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            try {
+                const sql = "SELECT model FROM products WHERE model = ?"
+                db.get(sql, [model], (err: Error | null, model: any) => {
+                    if (err) {
+						reject(err)
+						return;
+                    }
+					if (!model) {
+						reject(new ProductNotFoundError())
+						return;
+                    } else {
+						const sql = "DELETE FROM reviews WHERE model = ?"
+						db.run(sql, [model], (err: Error | null) => {
+							if (err) {
+								reject(err)
+								return;
+							}
+							else {
+								const sql = "DELETE FROM cartItems WHERE product_model = ?"
+								db.run(sql, [model], (err: Error | null) => {
+									if (err) {
+										reject(err)
+										return;
+									}
+									else {
+										const sql = "DELETE FROM products WHERE model = ?"
+										db.run(sql, [model], (err: Error | null) => {
+											if (err) {
+												reject(err)
+												return;
+											}
+											else {
+												resolve(true);
+												return;
+											}
+										})
+									}
+								})
+							}
+						})
+					}
+                })
+            } catch (error) {
+                reject(error)
+            }
+        })
 	}
-
-    /**
-     * Increases the available quantity of a product through the addition of new units.
-     * @param model The model of the product to increase.
-     * @param newQuantity The number of product units to add. This number must be added to the existing quantity, it is not a new total.
-     * @param changeDate The optional date in which the change occurred.
-     * @returns A Promise that resolves to the new available quantity of the product.
-     */
-    async changeProductQuantity(model: string, newQuantity: number, changeDate: string | null) :Promise<number> { 
-		return await this.dao.changeProductQuantity(model, newQuantity, changeDate)
+	
+	
+	deleteAllProducts(): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            try {
+                const sql = "DELETE FROM reviews"
+                db.run(sql, [], (err: Error | null) => {
+                    if (err) {
+						reject(err)
+						return;
+                    }
+                    else {
+						const sql = "DELETE FROM cartItems"
+						db.run(sql, [], (err: Error | null) => {
+							if (err) {
+								reject(err)
+								return;
+							}
+							else {
+								const sql = "DELETE FROM products"
+								db.run(sql, [], (err: Error | null) => {
+									if (err) {
+										reject(err)
+										return;
+									}
+									else {
+										resolve(true);
+										return;
+									}
+								})
+							}
+						})
+					}
+                })
+            } catch (error) {
+                reject(error)
+            }
+        })
 	}
-
-    /**
-     * Decreases the available quantity of a product through the sale of units.
-     * @param model The model of the product to sell
-     * @param quantity The number of product units that were sold.
-     * @param sellingDate The optional date in which the sale occurred.
-     * @returns A Promise that resolves to the new available quantity of the product.
-     */
-    async sellProduct(model: string, quantity: number, sellingDate: string | null) :Promise<boolean> {
-		return await this.dao.sellProduct(model, quantity, sellingDate)
-	}
-
-    /**
-     * Returns all products in the database, with the option to filter them by category or model.
-     * @param grouping An optional parameter. If present, it can be either "category" or "model".
-     * @param category An optional parameter. It can only be present if grouping is equal to "category" (in which case it must be present) and, when present, it must be one of "Smartphone", "Laptop", "Appliance".
-     * @param model An optional parameter. It can only be present if grouping is equal to "model" (in which case it must be present and not empty).
-     * @returns A Promise that resolves to an array of Product objects.
-     */
-    async getProducts(grouping: string | null, category: string | null, model: string | null) :Promise<Product[]> { 
-		return await this.dao.getProducts(grouping, category, model)
-	}
-
-    /**
-     * Returns all available products (with a quantity above 0) in the database, with the option to filter them by category or model.
-     * @param grouping An optional parameter. If present, it can be either "category" or "model".
-     * @param category An optional parameter. It can only be present if grouping is equal to "category" (in which case it must be present) and, when present, it must be one of "Smartphone", "Laptop", "Appliance".
-     * @param model An optional parameter. It can only be present if grouping is equal to "model" (in which case it must be present and not empty).
-     * @returns A Promise that resolves to an array of Product objects.
-     */
-    async getAvailableProducts(grouping: string | null, category: string | null, model: string | null) :Promise<Product[]> { 
-		return await this.dao.getAvailableProducts(grouping, category, model)
-	}
-
-    /**
-     * Deletes all products.
-     * @returns A Promise that resolves to `true` if all products have been successfully deleted.
-     */
-    async deleteAllProducts() :Promise <boolean> { 
-		return await this.dao.deleteAllProducts()
-	}
-
-
-    /**
-     * Deletes one product, identified by its model
-     * @param model The model of the product to delete
-     * @returns A Promise that resolves to `true` if the product has been successfully deleted.
-     */
-    async deleteProduct(model: string) :Promise <boolean> {
-		return await this.dao.deleteProduct(model)
-	}
-
 }
 
-export default ProductController;
+export default ProductDAO
