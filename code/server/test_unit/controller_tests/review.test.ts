faf4ae3898340cd6c178d4bb6e7a1e85ad365d5e
laurall974 <laura.llinares@edu.devinci.fr>
2024-06-15 13:38:31 +0200

    fix : controller review unit test

--- code/server/test_unit/controller_tests/review.test.ts
+++ code/server/test_unit/controller_tests/review.test.ts
@@ -1,9 +1,12 @@
 import { test, expect, jest, describe } from "@jest/globals";
 import ReviewController from "../../src/controllers/reviewController";
 import ReviewDAO from "../../src/dao/reviewDAO";
+import ProductDAO from "../../src/dao/productDAO";
 import { User } from "../../src/components/user";
+import { Product, Category } from "../../src/components/product";
 
 jest.mock("../../src/dao/reviewDAO");
+jest.mock("../../src/dao/productDAO");
 
 describe("ReviewController", () => {
     describe("addReview", () => {
@@ -14,6 +17,7 @@ describe("ReviewController", () => {
             const testComment = "Great product";
 
             // Mock the addReview method of the DAO
+            const mockGetProduct = jest.spyOn(ProductDAO.prototype, "getProductByModel").mockResolvedValueOnce(new Product(1200, testModel, Category.SMARTPHONE,"2023-01-01" , "Latest model with great features",5));
             const mockAddReview = jest.spyOn(ReviewDAO.prototype, "addReview").mockResolvedValueOnce(undefined);
 
             const controller = new ReviewController();
@@ -22,6 +26,10 @@ describe("ReviewController", () => {
             // Check if the addReview method of the DAO has been called once with the correct parameters
             expect(mockAddReview).toHaveBeenCalledTimes(1);
             expect(mockAddReview).toHaveBeenCalledWith(testModel, testUser, testScore, testComment);
+            expect(mockGetProduct).toHaveBeenCalledTimes(1);
+
+            mockGetProduct.mockRestore()
+
         });
     });
 });
@@ -29,22 +37,25 @@ describe("getProductReviews", () => {
     test("Returns reviews for a product", async () => {
         const testModel = "iphone";
         const expectedReviews = [
-            { model: "iphone", user: "user1", score: 5, date: "2023-01-01", comment: "Great product" },
-            { model: "iphone", user: "user2", score: 4, date: "2023-01-02", comment: "Good product" }
+            { model: testModel, user: "user1", score: 5, date: "2023-01-01", comment: "Great product" },
+            { model: testModel, user: "user2", score: 4, date: "2023-01-02", comment: "Good product" }
         ];
 
-        
+        const mockGetProduct = jest.spyOn(ProductDAO.prototype, "getProductByModel").mockResolvedValueOnce(new Product(1200, testModel, Category.SMARTPHONE,"2023-01-01" , "Latest model with great features",5));
         const mockGetProductReviews = jest.spyOn(ReviewDAO.prototype, "getProductReviews").mockResolvedValueOnce(expectedReviews);
 
         const controller = new ReviewController();
         const result = await controller.getProductReviews(testModel);
 
-    
         expect(mockGetProductReviews).toHaveBeenCalledTimes(1);
         expect(mockGetProductReviews).toHaveBeenCalledWith(testModel);
+        expect(mockGetProduct).toHaveBeenCalledTimes(1);
 
       
         expect(result).toEqual(expectedReviews);
+
+        mockGetProduct.mockRestore()
+
     });
     describe("deleteReview", () => {
         test("Deletes a review for a product", async () => {
@@ -63,11 +74,15 @@ describe("getProductReviews", () => {
         test("Deletes all reviews of a product", async () => {
             const testModel = "iphone";
 
+            const mockGetProduct = jest.spyOn(ProductDAO.prototype, "getProductByModel").mockResolvedValueOnce(new Product(1200, testModel, Category.SMARTPHONE,"2023-01-01" , "Latest model with great features",5));
             const mockDeleteReviewsOfProduct = jest.spyOn(ReviewDAO.prototype, "deleteReviewsOfProduct").mockResolvedValueOnce(undefined);
             const controller = new ReviewController();
             await controller.deleteReviewsOfProduct(testModel);
             expect(mockDeleteReviewsOfProduct).toHaveBeenCalledTimes(1);
             expect(mockDeleteReviewsOfProduct).toHaveBeenCalledWith(testModel);
+            expect(mockGetProduct).toHaveBeenCalledTimes(1);
+
+            mockGetProduct.mockRestore()
         });
     });
 
